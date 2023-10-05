82619b519eecf20df41d8407b31974e0ddf4e1da
Arseniy Pomazkov <arpomazkov@bk.ru>
2023-10-05 15:21:42 +0300

    Added parse_mode & disable_web_page_preview flags

--- index.js
+++ index.js
@@ -70,7 +70,10 @@ bot.on('callback_query:data', async (ctx) => {
 
   if (!callbackData.type.includes('option')) {
     const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
-    await ctx.reply(answer);
+    await ctx.reply(answer, {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true,
+    });
     await ctx.answerCallbackQuery();
     return;
   }
